e8f23ddf0542f29a6b218352f5b02be5e4692247
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2025-07-15 00:50:10 -0500

    Create Basic Website

---
+++ website/index.js
@@ -0,0 +1,11 @@
+document.getElementById("download-button").addEventListener("click", function(e) {
+    window.open("https://github.com/atticuscornett/archway/releases/latest", "_blank").focus();
+});
+
+document.getElementById("source-button").addEventListener("click", function(e) {
+    window.open("https://github.com/atticuscornett/archway", "_blank").focus();
+});
+
+document.getElementById("issue-button").addEventListener("click", function(e) {
+    window.open("https://github.com/atticuscornett/archway/issues", "_blank").focus();
+})
